4c9905ce2e7deee4e7c6394d5b938ed8133e2eb8
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-14 21:47:56 -0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Securely fetch the variables from environment
 BOT_TOKEN = os.getenv("7524278392:AAGd21uCuExw-JAuko9UC5l9GLrqmwy-mh4")
 MONGO_URL = os.getenv("mongodb+srv://NewFileShareBoT:EenaMeena@cluster0.7rbuejp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-ADMIN_ID = int(os.getenv("1490611763"))  # Ensure this is an integer
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # Ensure this is an integer
 WEBHOOK_URL = os.getenv("https://start-to-messages.onrender.com")  # Add this line for your webhook URL
 
 # Ensure all variables are set
