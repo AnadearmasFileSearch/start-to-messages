5611ee7af8acf8e9db6e0097ee30b8b6b45bad65
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-15 11:30:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
-# config.py
 import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env (only used in local development)
-load_dotenv()
-
-# Securely fetch the variables from environment
-BOT_TOKEN = os.getenv("7524278392:AAGd21uCuExw-JAuko9UC5l9GLrqmwy-mh4")
-MONGO_URL = os.getenv("mongodb+srv://NewFileShareBoT:EenaMeena@cluster0.7rbuejp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# Retrieve the environment variables
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URL = os.getenv("MONGO_URL")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))  # Ensure this is an integer
-WEBHOOK_URL = os.getenv("https://start-to-messages.onrender.com")  # Add this line for your webhook URL
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-# Ensure all variables are set
+# Print the values to check if they are correctly set
+print(f'BOT_TOKEN: {BOT_TOKEN}')
+print(f'MONGO_URL: {MONGO_URL}')
+print(f'ADMIN_ID: {ADMIN_ID}')
+print(f'WEBHOOK_URL: {WEBHOOK_URL}')
+
+# Raise an error if any variable is missing
 if not BOT_TOKEN or not MONGO_URL or not ADMIN_ID or not WEBHOOK_URL:
     raise ValueError("Missing required environment variables: BOT_TOKEN, MONGO_URL, ADMIN_ID, or WEBHOOK_URL.")
